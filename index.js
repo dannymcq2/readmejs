2a465e8ed8bb161f4a521a3481f99472d6fa392d
dannymcq2 <166949494+dannymcq2@users.noreply.github.com>
2024-07-09 00:12:05 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,94 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+function generateREADME(answers) {
+  return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+If you have any questions, you can reach me at [${answers.email}](mailto:${answers.email}). You can also find more of my work on [GitHub](https://github.com/${answers.github}).
+`;
+}
+
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateREADME(answers);
+  fs.writeFile('README.md', readmeContent, (err) => {
+    if (err) throw err;
+    console.log('README.md has been generated!');
+  });
+});
